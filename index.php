8318dd329cc633ba8637216c1dc17ea553c70529
unknown <triumphanyanga@gmail.com>
2024-04-08 14:28:53 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+    $v = 50;
+    $b = 200;
+    $g = "<h1>Triumph is the one coding...</h1>";
+echo "Hello World"; 
+
+
+// echo is used to display output in PHP.
+
+// echo $b + $v ;
+
+echo $g;
+
+
+?>
