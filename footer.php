fc09345ea2ca3436dd9b351c73abc7e207c01cd1
BigT <triumphanyanga@gmail.com>
2024-04-24 12:31:35 +0100

    login dashboard register

---
+++ footer.php
@@ -0,0 +1,3 @@
+<div class ="d-flex justify-content-center mt-4 bg-light">
+        <p>Designed by Steeze & Co 2024</p>
+</div>
---
+++ navbar.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./bootstrap-5.3.1-dist/css/bootstrap.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+
+    <title>Steeze App</title>
+    <style>
+        @import url(https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap);
+
+
+        :root {
+            --bg-color: #081b29;
+            --second-color: #112e42;
+            --text-color: #ededed;
+            --main-color: #00abf0;
+        }
+
+        .h3 {
+            color: var(--bg-color);
+        }
+
+        .span {
+            color: var(--main-color);
+        }
+    </style>
+</head>
+
+<body>
+    <div class="company">
+        <!-- <h3>Operate in <span>STEEZE</span></h3> -->
+
+    </div>
+    <div class="d-flex justify-content-center my-5 bg-dark p-3">
+        <a href="index.php" class="btn btn-light me-2">Register</a>
+        <a href="login.php" class="btn btn-light me-2">Login</a>
+        <a href="deposit.php" class="btn btn-light me-2">Deposit</a>
+        <a class="btn btn-light me-2">Withdraw</a>
+        <a class="btn btn-light me-2">History</a>
+    </div>
+</body>
+
+</html>
